b0d0c19e7333f548f6a7b87d2701b30c68d0ef28
a2913987 <thomas.brown01@student.adelaide.edu.au>
2026-05-26 21:44:38 +0800

    DOC(Edge): add Javadoc for public methods

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -9,52 +9,108 @@
  *    the University's Academic Integrity Policy.
  **/
 
+
+
+/**
+ * Edge defines a connection between two vertices in a graph.
+ */
 public class Edge {
     private int source;
     private int dest;
     private double weight;
 
+    /**
+     * Create edge from source vertex to destination vertex.
+     *
+     * @param source source vertex
+     * @param dest destination vertex
+     */
     public Edge(int source, int dest) {
         this.source = source;
         this.dest = dest;
         this.weight = weight;
     }
 
-
+    /**
+     * Get source vertex.
+     *
+     * @return source vertex
+     */
     public int getSource() {
         return source;
     }
 
+    /**
+     * Get destination vertex.
+     *
+     * @return destination vertex
+     */
     public int getDest() {
         return dest;
     }
 
+    /**
+     * Get edge weight.
+     *
+     * @return edge weight
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Set source vertex.
+     *
+     * @param source source vertex
+     */
     public void setSource(int source) {
         this.source = source;
     }
 
+    /**
+     * Set destination vertex.
+     *
+     * @param dest destination vertex
+     */
     public void setDest(int dest) {
         this.dest = dest;
     }
 
+    /**
+     * Set edge weight.
+     *
+     * @param weight edge weight
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * Compare edge with another edge.
+     *
+     * @param other edge to compare
+     * @return true if source and destination match
+     */
     public boolean equals(Edge other) {
         return this.source == other.source
                 && this.dest == other.dest;
     }
 
+    /**
+     * Return hash code for edge.
+     *
+     * @return hash code
+     */
     @Override
     public int hashCode() {
         return source + dest + (int) weight;
     }
 
+    /**
+     * Return string version of this edge.
+     *
+     * @return edge as string
+     */
     @Override
     public String toString() {
         return "" + source + dest;
