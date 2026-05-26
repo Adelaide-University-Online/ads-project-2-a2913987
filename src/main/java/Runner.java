3ffec334a9f9191091bab01dd48b6cd0fbf50738
a2913987 <thomas.brown01@student.adelaide.edu.au>
2026-05-26 21:44:38 +0800

    DOC(Runner): Update file header

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,13 +1,14 @@
 /**
-* File: filename.java
-* Description: A brief description of this Java module.
-* Author: Steve Jobs
-* Student ID: 12345678
-* Email ID: jobst007
-* AI Tool Used: Y/N (This includes all AI Tools e.g. ChatGPT, Microsoft or Github Copiliot etc... Please leave blank if you do not wish to share this information)
+* File: Runner.java
+* Description:
+* Author: Thomas Brown
+* Student ID: a2913987
+* Email ID: thomas.brown01
+* AI Tool Used: Y
 * This is my own work as defined by
 *    the University's Academic Integrity Policy.
 **/
+
 public class Runner {
     
     public static void main(String[] args) {
