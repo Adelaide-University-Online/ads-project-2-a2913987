474328946b52dfc651ef8fd9562d389ce3c78d7b
a2913987 <thomas.brown01@student.adelaide.edu.au>
2026-05-28 19:36:35 +0800

    FEAT(AdjacencyListGraph): add equals, hashCode and toString

--- src/main/java/AdjacencyListGraph.java
+++ src/main/java/AdjacencyListGraph.java
@@ -33,6 +33,66 @@ public class AdjacencyListGraph extends AbstractGraph {
         for (int i = 0; i < numV; i++) {
             edges[i] = new LinkedList<Edge>();
         }
-
     }
+
+    /**
+     * Compare graph with another graph object.
+     *
+     * @param obj object to compare
+     * @return true if graphs are the same
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof AdjacencyListGraph)) {
+            return false;
+        }
+        AdjacencyListGraph other = (AdjacencyListGraph) obj;
+        if (getNumV() != other.getNumV() || isDirected() != other.isDirected()) {
+            return false;
+        }
+
+        // Check that each adjacency list matches
+        for (int i = 0; i < getNumV(); i++) {
+            if (!edges[i].equals(other.edges[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Return hash code for graph.
+     *
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int result = 42 * getNumV() + (isDirected() ? 1 : 0);
+
+        // Include adjacency list in hash code
+        for (List<Edge> list : edges) {
+            result = 42 * result + list.hashCode();
+        }
+
+        return result;
+    }
+
+    /**
+     * Return string version of this graph.
+     *
+     * @return graph as string
+     */
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < getNumV(); i++) {
+            output.append(i).append(": ").append(edges[i]).append("\n");
+        }
+        return output.toString();
+    }
+
 }
