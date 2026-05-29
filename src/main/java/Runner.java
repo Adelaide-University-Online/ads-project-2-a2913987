119a60e8882b64dc881e02b47efa5f2b2470511c
a2913987 <thomas.brown01@student.adelaide.edu.au>
2026-05-29 20:28:02 +0800

    FEAT(Runner): add basic entry point to read and print

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,21 +1,32 @@
 /**
-* File: Runner.java
-* Description:
-* Author: Thomas Brown
-* Student ID: a2913987
-* Email ID: thomas.brown01
-* AI Tool Used: Y
-* This is my own work as defined by
-*    the University's Academic Integrity Policy.
-**/
+ * File: Runner.java
+ * Description:
+ * Author: Thomas Brown
+ * Student ID: a2913987
+ * Email ID: thomas.brown01
+ * AI Tool Used: Y
+ * This is my own work as defined by
+ *    the University's Academic Integrity Policy.
+ **/
+
+import java.io.FileNotFoundException;
 
 public class Runner {
     
     public static void main(String[] args) {
-        System.out.println("Welcome to ADS Assignment Starter!");
-        System.out.println("This is a basic Java project template.");
-        System.out.println("You can modify this file to implement your assignment requirements.");
-        
+        String filename = "example_file_XBDA.txt";
+
+        DegreeGraphBuilder gbuild = new DegreeGraphBuilder();
+
+        try {
+            gbuild.read(filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find: " + filename);
+            return;
+        }
+
+        System.out.println(gbuild.getCourseCount());
+        System.out.println(gbuild.getCourses());
+        System.out.println(gbuild.getGraph());
     }
-    
 }
